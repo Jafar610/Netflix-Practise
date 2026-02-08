139a655e92ad79fce6f72a405f9a9c3ae811a6c0
Jafar610 <jafarmohamed955@gmail.com>
2026-02-08 21:59:18 +0300

    file updated as well as removed

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,29 +1,50 @@
-import React, {useState, useEffect} from 'react'
-import './header.css'
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import "./header.css";
 function Header() {
-    const [show, setShow] = useState(false);
-        useEffect(() => {
-            window.addEventListener('scroll', () => {
-                if (window.scrollY > 100) {
-                    setShow(true);
-                } else setShow(false);
-            })
-        }, []);
-
   return (
-    <div className={`nav ${show && "nav__black"}`}>
+    <>
+      <div className="header">
+        <div className="nav__bar">
+            <div>
             <img
-                className="nav__logo"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
-                alt="Netflix Logo"
-            />
-            <img
-                src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
-                alt="Avatar logo"
-                className="nav__avatar"
-            />
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt="Netflix Logo"
+          />
+            </div>
+          
+          <div>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#">TV Show</a>
+            </li>
+            <li>
+              <a href="#">Movies</a>
+            </li>
+            <li>
+              <a href="#">New & Popular</a>
+            </li>
+            <li>
+              <a href="#">My List</a>
+            </li>
+            <li>
+              <a href="#">Browse My Language</a>
+            </li>
+          </div>
+          <div className="icons">
+            <SearchIcon className="header__searchIcon" />
+            <NotificationsIcon className="header__notificationIcon" />
+            <AccountCircleIcon className="header__accountIcon" />
+            <ArrowDropDownIcon className="header__dropdownIcon" />
+          </div>
         </div>
-  )
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
