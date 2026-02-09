23c0b41f6fd13c9930cd78fef2a02ea22996d24a
Jafar610 <jafarmohamed955@gmail.com>
2026-02-09 23:27:01 +0300

    file updated

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,56 +1,26 @@
-import React, { useState, useEffect } from "react";
-import "./banner.css";
-import axios from "../MovieData/axios";
-import requests from "../MovieData/requests";
-
+import React from 'react'
+import './banner.css'
 function Banner() {
-  const [movie, setMovie] = useState({});
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const request = await axios.get(requests.fetchNetflixOriginals);
-        setMovie(
-          request.data.results[
-            Math.floor(Math.random() * request.data.results.length)
-          ],
-        );
-      } catch (error) {
-        console.log("Error fetching Netflix Originals:", error);
-      }
-    })();
-  }, []);
-
-  function truncate(str, n) {
-    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-  }
-
   return (
-    <>
-      <div
-        className="banner"
-        style={{
-          backgroundSize: "cover",
-          backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
-          backgroundPosition: "center",
-        }}
-      >
-        <div className="banner__contents">
-          <h1 className="banner__title">
-            {movie?.title || movie?.name || movie?.original_name}
-          </h1>
-          <div className="banner__buttons">
-            <button className="banner__button">Play</button>
-            <button className="banner__button">My List</button>
-          </div>
-          <h1 className="banner__description">
-            {truncate(movie?.overview, 150)}
-          </h1>
+    <div className="banner" 
+    style={{backgroundImage: "url('https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO/AAAABXoQh8n7qHkKZyLh8iVZtqjvN8sJr9l3aGgWz5u4e7n1b2v5s9wX9z5l3.jpg?r=1c0.jpg')",
+      backgroundSize: "cover",
+      backgroundPosition: "center center"
+
+    }}>
+      <div className="banner_content">
+        <h1 className="banner_title">Stranger Things</h1>
+        <div className="banner_buttons">
+          <button className="banner_button play">Play</button>
+          <button className="banner_button info">More Info</button>
         </div>
-        <div className="banner__fadeBottom" />
+        <p className="banner_description">
+          When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces and one strange little girl.
+        </p>
+        <div className="fade--bottom"></div>
       </div>
-    </>
-  );
+    </div>
+  )
 }
 
-export default Banner;
+export default Banner
